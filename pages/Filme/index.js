1cac5143d562b5288d6d800f72ce1b2fd833d6a3
Gustavo Lofrano <161264484+GustavoHenriqueLofrano@users.noreply.github.com>
2025-05-21 18:01:46 -0300

    Add files via upload

---
+++ pages/Filme/index.js
@@ -0,0 +1,8 @@
+function Filme() {
+    return(
+    <div>
+        <h1>Teste pagina Filmes</h1>
+    </div>
+)}
+
+export default Filme;
---
+++ pages/Home/index.js
@@ -0,0 +1,10 @@
+import './home.css'
+
+function Home() {
+    return(
+    <div>
+        <h1>Teste Home</h1>
+    </div>
+)}
+
+export default Home;
