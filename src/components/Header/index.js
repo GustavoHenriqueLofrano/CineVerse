c87f92138076599897ce7cd906a2a36584d39482
gustavohenriquelofrano <gustavohenriquelofrano@hotmail.com>
2025-05-31 11:26:56 -0300

    att

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function Header(){
   return(
     <header>
-      <Link className="logo" to="/">Teste</Link>
+      <Link className="logo" to="/">Teste Git</Link>
       <Link className="favoritos" to="/favoritos">Meus filmes</Link>
     </header>
   )
