bd9a5a4397a4e3663771a3869fa900ab49efd4ff
Gustavo Lofrano <gustavohenriquelofrano@hotmail.com>
2025-06-09 10:36:55 -0300

    ...

--- src/pages/MeusFilmes/index.js
+++ src/pages/MeusFilmes/index.js
@@ -1,43 +1,53 @@
-import '../MeusFilmes/index'
-import {useEffect, useState} from 'react'
+import '../MeusFilmes/index.css'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
-function MeusFilmes(){
+function MeusFilmes() {
     const [filmes, setFilmes] = useState([]);
 
-        useEffect(() =>{
-            const myList = localStorage.getItem("@CineVerse");
+    useEffect(() => {
+        const myList = localStorage.getItem("@CineVerse");
 
-            setFilmes(JSON.parse(myList) || [])
-        },[])
+        setFilmes(JSON.parse(myList) || [])
+    }, [])
 
-    return(
-        <div>
-            <h1 className='my-filmes'>Meus filmes</h1>
-               <ul>
+    function ExcluirFilme(id) {
+        let FilterMovies = filmes.filter((item) => {
+            return (item.id !== id)
+        })
+
+        setFilmes(FilterMovies);
+
+        localStorage.setItem("@CineVerse", JSON.stringify(FilterMovies));
+
+    }
+    if (filmes.length === 0) {
+        return (
+            <span className='empty-list'>Sua Lista De Filmes Está Vazia... </span>
+        )
+    }
+
+    return (
+        <div className='my-movies'>
+            <h1>Meus filmes</h1>
+            <ul>
                 {filmes.map((item) => {
-                    return(
+
+                    return (
                         <li key={item.id}>
-                        <span>{item.title}</span>
+                            <span>{item.title}</span>
                             <div>
                                 <Link to={`/filme/${item.id}`}>detalhes</Link>
-                                
-                                <button>excluir</button>
-                            
+
+                                <button onClick={() => ExcluirFilme(item.id)}>excluir</button>
+
                             </div>
                         </li>
                     )
 
 
                 })}
-
-
-
-               </ul>
-                 
-
-            
-
+            </ul>
         </div>
     )
 }
